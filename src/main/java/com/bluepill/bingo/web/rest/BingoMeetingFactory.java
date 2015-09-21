83763c92ef2ca07b3a5d8df22dcbbb4fe7d69a9e
csbiggar <tddp@localhost.localdomain>
2015-09-21 12:32:23 +0000

    Putting hard coded image urls into the products response

--- src/main/java/com/bluepill/bingo/web/rest/BingoMeetingFactory.java
+++ src/main/java/com/bluepill/bingo/web/rest/BingoMeetingFactory.java
@@ -4,6 +4,7 @@ import com.bluepill.bingo.web.rest.dto.bingo.Game;
 import com.bluepill.bingo.web.rest.dto.bingo.Product;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -13,9 +14,12 @@ public class BingoMeetingFactory {
     Game game;
 
     public BingoMeetingFactory() {
-        List<Product> products = new LinkedList<>();
-        for (int i = 0; i < 90; i++) {
-            products.add(new Product(String.valueOf(i), "img"+i));
+
+        List<Product> products = new ArrayList<Product>();
+
+        for ( int i = 0; i< 90 ;i++ ) {
+            Product product  = new Product( String.valueOf(i+1), "http://johnlewis.scene7.com/is/image/JohnLewis/23444402" + i);
+            products.add(product);
         }
 
         game = new Game(products, 1);
