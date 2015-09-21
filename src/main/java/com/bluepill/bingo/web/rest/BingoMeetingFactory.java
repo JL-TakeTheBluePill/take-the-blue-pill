a5bc5e846d346e25a1302fc73a52cc3d546f1e2e
properdom <tddp@localhost.localdomain>
2015-09-21 15:03:30 +0000

    wire up api

--- src/main/java/com/bluepill/bingo/web/rest/BingoMeetingFactory.java
+++ src/main/java/com/bluepill/bingo/web/rest/BingoMeetingFactory.java
@@ -3,9 +3,6 @@ package com.bluepill.bingo.web.rest;
 import com.bluepill.bingo.web.rest.dto.bingo.Game;
 import com.bluepill.bingo.web.rest.dto.bingo.Product;
 import com.bluepill.bingo.web.rest.dto.bingo.SearchApiResponse;
-import groovyx.net.http.ContentType;
-import groovyx.net.http.HttpResponseDecorator;
-import groovyx.net.http.RESTClient;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -53,8 +50,6 @@ public class BingoMeetingFactory {
 
         SearchApiResponse response = restTemplate.getForObject("https://api.johnlewis.com/v1/products/search?q=blue&key=7xgtieKjAlGeGVUef2KgD2yktbtuVqwx&pageSize=90", SearchApiResponse.class);
 
-  //     System.out.println(response);
-
         int productNum = 1;
         List<Product> products = response.getProducts();
         for (Product product : products) {
@@ -63,59 +58,4 @@ public class BingoMeetingFactory {
 
         return response.getProducts();
     }
-
-
-    List<Product> getProductsBySearchHttpClient() throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException, URISyntaxException {
-
-        RESTClient api = new RESTClient("https://api.johnlewis.com/v1/products/search?q=blue&key=7xgtieKjAlGeGVUef2KgD2yktbtuVqwx&pageSize=90");
-        //Use JVM properties for this if set, as this won't work if we run this from FAT
-        api.ignoreSSLIssues();
-        //  api.setProxy("localhost", 3128, "http");
-
-        Map<String, String> headers = new HashMap<String, String>();
-        headers.put("User-Agent", "Wget/1.12 (linux-gnu)");
-
-        api.setHeaders(headers);
-
-        HttpResponseDecorator response = null;
-        Map<String, Object> getStuff = new HashMap<String, Object>();
-        getStuff.put("path", "");
-        getStuff.put("contentType", ContentType.ANY);
-
-        try {
-            response = (HttpResponseDecorator) api.get(getStuff);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-        System.out.println("  ");
-        System.out.println("  ");
-        System.out.println("  ");
-        System.out.println("**------- RESPONSE STATUS -------**");
-
-        if (response != null) {
-            System.out.println(response.getStatus());
-            System.out.println("  ");
-            System.out.println("  ");
-            System.out.println("  ");
-            System.out.println(" -------- DATA ------- ");
-            System.out.println(response.getData());
-
-        } else {
-            System.out.println("RESPONSE STILL NULL");
-        }
-//
-//        sysout '**------- RESPONSE STATUS -------**'
-//        println response.status
-//        println '**------- RESPONSE CONTENT TYPE -------**'
-//        println response.contentType
-//        println '**---------- DATA --------**'
-//        println response.data
-//        println '**---------- END --------**'
-
-        return null;
-
-    }
-
 }
